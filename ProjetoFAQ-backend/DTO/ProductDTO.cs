1b63ccbedfa8f38cdedd8b08e9f2f541fc6feb84
Andre <andresantoscarvalho2004@gmail.com>
2025-06-05 15:26:38 -0300

    Adicionando Frontend na branch frontend

--- ProjetoFAQ-backend/DTO/ProductDTO.cs
+++
@@ -1,9 +0,0 @@
-// ReSharper disable all
-namespace ProjetoFAQ.DTO;
-
-public class ProductDTO
-{
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public decimal Price { get; set; }
-}
